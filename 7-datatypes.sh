8567e0b17ac84436e72a534bf51016042d9f73a4
vamsi krishna <vamsi.562@gmail.com>
2026-04-15 11:48:27 +0530

    7-datatypes.sh

---
+++ 7-datatypes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# this script demonstrates about datatypes in bash
+
+no1=10
+no2=20
+
+$sum=$((no1 + no2))
+
+echo "The sum of $no1 and $no2 is: $sum"
+
+name="John Doe"
+echo "My name is $name"
+
+Names=("$1" "$2" "$3")
+echo "The names are: ${Names[0]}, ${Names[1]}, and ${Names[2]}"
+echo " names are ${Names[@]}"
+echo "Number of names: ${#Names[@]}" # #-to get no of elements in array
