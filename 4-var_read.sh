ae32c2ba6763877181bcc2df3542770a015f5b0a
vamsi krishna <vamsi.562@gmail.com>
2026-04-15 11:25:47 +0530

    4-var_read.sh using s flag

--- 4-var_read.sh
+++ 4-var_read.sh
@@ -5,3 +5,8 @@ echo "enter your pin:"
 read pin_number 
 
 echo "your pin number is $pin_number"
+
+# passing variables using read command with -s option to hide the input
+echo "enter your name:"
+read -s name
+echo "your name is $name"
