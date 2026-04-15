442fe154b7aae13e223025846d0e62ee9b677f42
vamsi krishna <vamsi.562@gmail.com>
2026-04-15 12:57:29 +0530

    11-colors.sh

--- 11-colors.sh
+++ 11-colors.sh
@@ -2,11 +2,11 @@
 
 # this script demonstrates about colors in bash scripting
 
-R="\e[31m]"
-G="\e[32m]"
-Y="\e[33m]"
-W="\e[0m]"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+W="\e[0m"
 
-echo -e "$R This is red color.$W"
-echo -e "$G This is green color.$W"
-echo -e "$Y This is yellow color.$W"
+echo -e "$R This is red color $W"
+echo -e "$G This is green color$W"
+echo -e "$Y This is yellow color$W"
