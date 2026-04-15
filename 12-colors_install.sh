f5d60e8dc5bf5649303aa5ca59a42eba695e1412
vamsi krishna <vamsi.562@gmail.com>
2026-04-15 13:55:01 +0530

    12-colors_install.sh

--- 12-colors_install.sh
+++ 12-colors_install.sh
@@ -20,27 +20,27 @@ validate_install() {
 
 dnf list installed mysql
 if [ $? -ne 0 ]; then
-    echo -e "${Y}MySQL is not installed. Installing MySQL...${W}"
+    echo -e "${Y} MySQL is not installed. Installing MySQL... ${W}"
     dnf install mysql -y
     validate_install $? "MySQL"
 else
-    echo -e "${G}MySQL is already installed.${W}"
+    echo -e "${G} MySQL is already installed.${W}"
 fi
 
 dnf list installed nginx
 if [ $? -ne 0 ]; then
-    echo -e "${Y}Nginx is not installed. Installing Nginx...${W}"
+    echo -e "${Y} Nginx is not installed. Installing Nginx... ${W}"
     dnf install nginx -y
     validate_install $? "Nginx"
 else
-    echo -e "${G}Nginx is already installed.${W}"
+    echo -e "${G} Nginx is already installed.${W}"
 fi  
 
 dnf list installed python3
 if [ $? -ne 0 ]; then
-    echo -e "${Y}Python 3 is not installed. Installing Python 3... ${W}"
+    echo -e "${Y} Python 3 is not installed. Installing Python 3...  ${W}"
     dnf install python3 -y
     validate_install $? "Python 3"
 else
-    echo -e "${G}Python 3 is already installed.${W}"
+    echo -e "${G} Python 3 is already installed. ${W}"
 fi  
