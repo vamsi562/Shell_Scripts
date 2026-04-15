0cb8f1833fc066762e8d428da848774afb5740eb
vamsi krishna <vamsi.562@gmail.com>
2026-04-15 12:28:35 +0530

    9-script_exec.sh

---
+++ 9-script_exec.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# this script demonstrates installing packages if you user have root access
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "You need root access to run this script."
+    exit 1
+fi
+
+dnf install python3 -y
+echo "Python 3 has been installed successfully."
+
+if [ $? -eq 0 ]; then
+    echo "Python 3 installation was successful."
+else
+    echo "Python 3 installation failed."
+fi
