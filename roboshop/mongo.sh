458e041bfdefbe58ce8a2c97cab35e56b7851cc2
vamsi krishna <vamsi.562@gmail.com>
2026-05-10 14:34:29 +0530

    updated mongo db

---
+++ roboshop/mongo.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" 
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Error: run this script as root user $N" | tee -a $LOG_FILE
+    exit 1
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enable MongoDB"
+
+systemctl start mongod 
+VALIDATE $? "Start MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
+
+systemctl restart mongod
+VALIDATE $? "Restarted MongoDB
---
+++ roboshop/roboshop.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# this script is used to install servers for implementing roboshop application
+
+AMI_ID="ami-0220d79f3f480ecf5"
+INSTANCE_TYPE="t3.micro"
+SECURITY_GROUP_ID="sg-0409773d198d0da2f"
+Zone_ID="Z0443476BR0YLVN9TX31"
+Domain_Name="chikoo.fun"
+
+for i in $@; do
+    TAG_NAME="roboshop-$i"
+    echo "Creating EC2 instance for $i with tag $TAG_NAME..."
+    Instance_ID=$(aws ec2 run-instances \
+        --image-id "$AMI_ID" \
+        --instance-type "$INSTANCE_TYPE" \
+        --security-group-ids "$SECURITY_GROUP_ID" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$TAG_NAME}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text)
+    # Get Private IP
+    if [ $i != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$i.$DOMAIN_NAME" # mongodb.chikoo.fun
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" # chikoo.fun
+    fi
+    echo "$i: $IP"
+    # Create Route53 Record
+    echo "Creating Route53 record for $i with IP $IP..."
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id "$Zone_ID" \
+    --change-batch "{
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }"
+done
