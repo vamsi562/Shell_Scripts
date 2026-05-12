c25952cbcaca30f2cf97f239bed39d42174e6325
vamsi krishna <vamsi.562@gmail.com>
2026-05-12 20:21:49 +0530

    added roboshop shell script

--- roboshop/roboshop_upd.sh
+++ roboshop/roboshop_upd.sh
@@ -11,7 +11,7 @@ DOMAIN_NAME="chikoo.fun"
 for instance in "$@" # mongodb redis mysql
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id "$AMI_ID" --instance-type t3.micro --security-group-ids "$SG_ID" --subnet-id "$SUBNET_ID" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
-    if [ "$instance" != "frontend" ]; then
+    if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
     else
@@ -20,15 +20,9 @@ do
     fi
     echo "$instance: $IP"
     
-    aws route53 list-resource-record-sets \
+    aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
-    --query "ResourceRecordSets[?Name == '$RECORD_NAME.' && Type == 'A']"
-    
-    if [ $? -ne 0 ]; then
-        echo "Record set $RECORD_NAME does not exist. Creating a new record set."
-        aws route53 change-resource-record-sets \
-        --hosted-zone-id $ZONE_ID \
-        --change-batch '
+    --change-batch '
         {
             "Comment": "Updating record set"
             ,"Changes": [{
@@ -43,42 +37,5 @@ do
             }
             }]
         }
-        '
-    else
-        echo "Record set $RECORD_NAME already exists. deleting existing record and creating new one."
-        echo "Deleting existing record set $RECORD_NAME"
-        aws route53 change-resource-record-sets \
-        --hosted-zone-id $ZONE_ID \
-        --change-batch "{
-                    \"Changes\": [
-               {
-                     \"Action\": \"DELETE\",
-                     \"ResourceRecordSet\": $(aws route53 list-resource-record-sets \
-                      --hosted-zone-id $ZONE_ID \
-                      --query "ResourceRecordSets[?Name == '$RECORD_NAME.']" \
-                      --output json | jq '.[0]')
-               }
-            ]
-        }"
-        
-        echo "Creating new record set $RECORD_NAME with IP $IP"
-        aws route53 change-resource-record-sets \
-        --hosted-zone-id $ZONE_ID \
-        --change-batch '
-        {
-            "Comment": "Updating record set"
-            ,"Changes": [{
-            "Action"              : "UPSERT"
-            ,"ResourceRecordSet"  : {
-                "Name"              : "'"$RECORD_NAME"'"
-                ,"Type"             : "A"
-                ,"TTL"              : 1
-                ,"ResourceRecords"  : [{
-                    "Value"         : "'"$IP"'"
-                }]
-            }
-            }]
-        }
-        '
-    fi
+    '
 done
