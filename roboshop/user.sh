fca13875f4071ea209f8da45bba781df5d84d444
vamsi krishna <vamsi.562@gmail.com>
2026-05-13 19:00:51 +0530

    user shell script added

---
+++ roboshop/user.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+trap '"echo -e script failed at line no: $LINENO, command: $BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+# MONGODB_HOST="mongodb.chikoo.fun"
+SCRIPT_DIR=$PWD
+START_TIME=$(date +%s)
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Error: run this script as root user $N" | tee -a $LOG_FILE
+    exit 1
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disable NodeJS module"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enable NodeJS 20 module"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJS"
+
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Adding roboshop user"
+else
+    echo -e "roboshop user already exists ... $Y SKIPPING $N"
+fi
+
+mkdir -p /app &>>$LOG_FILE
+
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading user code"
+cd /app 
+unzip /tmp/user.zip &>>$LOG_FILE
+VALIDATE $? "Extracting user code"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "Installing user dependencies"
+
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service &>>$LOG_FILE
+VALIDATE $? "Copying user service file"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Reloading systemd daemon" 
+
+systemctl enable user &>>$LOG_FILE
+VALIDATE $? "Enabling user service"
+systemctl start user &>>$LOG_FILE
+VALIDATE $? "Starting user service"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
