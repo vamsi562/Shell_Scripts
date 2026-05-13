382242aff17944282933215b4abea731c0024819
vamsi krishna <vamsi.562@gmail.com>
2026-05-13 11:46:12 +0530

    mongodb shell script

--- roboshop/mongo.sh
+++ roboshop/mongo.sh
@@ -44,4 +44,4 @@ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections to MongoDB"
 
 systemctl restart mongod &>>$LOG_FILE
-VALIDATE $? "Restarted MongoDB
+VALIDATE $? "Restarted MongoDB"
