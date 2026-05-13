d54081a3562bc50d200fc34e7a992fd0eada89de
vamsi krishna <vamsi.562@gmail.com>
2026-05-13 19:39:52 +0530

    front end  shell script added

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -67,4 +67,4 @@ VALIDATE $? "Restarting nginx service"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N" #
